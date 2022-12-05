2f6909ddd6c35bfa37f6a14f4ee2fe14d1724e75
Erika Machado <erikafirmino.ads@gmail.com>
2022-12-05 09:05:03 -0300

    arquivos novos

---
+++ aula03/index.js
@@ -0,0 +1,10 @@
+console.log("Erika 'Machado'"); 
+console.log('Erika "Machado"'); 
+console.log(`'Erika' "Machado"`); 
+
+console.log(35, 15.85, ',erika machado');
+
+//String = Texto
+// Number = Número
+
+console.log('Meu nome é "Erika Machado". Estou aprendendo JavaScript às', 10, "da manhã.")
---
+++ aula04/index.js
@@ -0,0 +1,16 @@
+// comentário de linha
+// E escrever um comentário 
+
+console.log('Olá Mundo');
+
+/*
+comentário de blocos
+
+console.log('linha 1');
+console.log('linha 2');
+console.log('linha 3');
+console.log('linha 4'); 
+
+*/
+
+
