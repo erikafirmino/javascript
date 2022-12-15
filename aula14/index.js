b13677535644aa86b1a5a8bdb794b92c65ac4b6f
Erika Machado <erikafirmino.ads@gmail.com>
2022-12-15 18:19:59 -0300

    att

---
+++ aula14/index.js
@@ -0,0 +1,49 @@
+/*
+let num1 = 1;
+let num2 = 2.5;
+*/
+//console.log(num1.toString() + num2);
+
+// Altera o tipo da variavel de Number -> String
+
+/*
+num1=num1.toString();
+console.log(typeof num1);
+*/
+
+// ".toString(2)" -- Mostra a representação binaria.
+
+//".toFixed(2)" -- Para especificar as casas decimais.
+
+//"Number.isInteger(num1)" -- Para saber se o número é intero (retorna true ou false).
+
+//"Number.isNaN()" -- Para saber se a conta é invalida (retorna true ou false).
+
+/*
+let num1 = 0.7;
+let num2 = 0.1;
+
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+
+num1 = parseFloat(num1.toFixed(2));
+
+console.log(num1);
+console.log(Number.isInteger(num1));
+*/
+
+let num1 = 0.7;
+let num2 = 0.1;
+
+num1 += num2;
